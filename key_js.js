9f09ea43c1887e3b0efee630aa64f36606403824
shayanfpg9 <shfqaen@gmail.com>
2023-01-18 15:50:44 +0330

    basic function

--- key_js.js
+++ key_js.js
@@ -1,3 +1,58 @@
-js_key = {
-        new_js_key : function (js_key_name,js_key_function,js_key_function_input){document.addEventListener('keydown', (event) => {if (event.key===js_key_name){js_key_function(js_key_function_input)}},false)}
-    }
+function key(element, props = {}) {
+  // when use as function without parameter
+  if (
+    typeof element === "object" &&
+    Object.hasOwnProperty.call(element, "keys")
+  ) {
+    if (typeof props == "string") {
+      props = {};
+      props.on = props;
+    } else if (typeof props !== "object") props = {};
+
+    props.keys = element;
+    element = undefined;
+  }
+  if (element == undefined) element = window;
+
+  // default value of props.on
+  if (props?.on == undefined) props.on = "press";
+
+  // run event listener if we have some keys
+  if (props?.keys !== undefined && typeof props.keys == "object") {
+    element.addEventListener(`key${props.on}`, (event) => {
+      for (const key in props.keys) {
+        if (key == "any") {
+          props.keys[key](event);
+          message(`key ${event.key} pressed\non event "${key}"`);
+        } else if (
+          event.key == key ||
+          event.code == key ||
+          event.keyCode == key
+        ) {
+          props.keys[key](event);
+          message(`key ${event.key} pressed\non event "${key}"`);
+        }
+      }
+    });
+  }
+}
+
+function message(msg) {
+  // colorful console.log
+  console.log(`->%c${msg}`, "margin-left:1rem;font-family:helvetica;");
+}
+
+// global init
+Window.prototype.key = (keys, on) => {
+  key(window, { keys, on });
+};
+
+// init for document
+document.key = (keys, on) => {
+  key(document, { keys, on });
+};
+
+// init for all of the elements
+Element.prototype.key = function (keys, on) {
+  key(this, { keys, on });
+};
